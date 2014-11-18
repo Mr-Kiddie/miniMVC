42dfc26be1cc7dfc31f7a576a2549d9121b6d0c3
scriptkiddie <zengxian@scriptkiddie.me>
2014-11-18 18:09:13 +0800

    bug fixedO

--- testController.class.php
+++ testController.class.php
@@ -2,7 +2,7 @@
 	class testController {
 		function show() {
 			$testModel = new testModel();
-			$data = testModel->get();
+			$data = $testModel->get();
 			$testView = new testView();
 			$testView->display($data);
 		}
